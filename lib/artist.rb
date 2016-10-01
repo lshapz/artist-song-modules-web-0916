12a6b9d570feac4ede77bf71be5852c4c900ca3d
lshapz <laurashapiro01@gmail.com>
2016-10-01 17:44:08 -0400

    Done.

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,43 +1,45 @@
 require 'pry'
+# require_relative '../lib/concerns/memorable'
+# require_relative '../lib/concerns/findable'
+
 
 class Artist
   attr_accessor :name
   attr_reader :songs
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
 
   @@artists = []
 
-  def self.find_by_name(name)
-    @@artists.detect{|a| a.name == name}
-  end
-
   def initialize
-    @@artists << self
-    @songs = []
+      super
+      @songs = []
   end
+  #adds the new artist to the artist array, sets an empty array of songs for new artist
 
   def self.all
     @@artists
   end
 
-  def self.reset_all
-    self.all.clear
-  end
-
-  def self.count
-    @@artists.count
-  end
+  #lists all the artist we've ever made
 
   def add_song(song)
     @songs << song
     song.artist = self
   end
+  #adds a new song and sets the song's artist to itself
 
   def add_songs(songs)
     songs.each { |song| add_song(song) }
   end
+  #same thing but with multiple, probs as an array
 
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
+  #looks like it's making something about the filename
+
 
 end
--- lib/song.rb
+++ lib/song.rb
@@ -1,36 +1,36 @@
 require 'pry'
+require_relative '../lib/concerns/memorable'
+require_relative '../lib/concerns/findable'
 
 class Song
   attr_accessor :name
   attr_reader :artist
+  extend Memorable::ClassMethods
+  extend Findable::ClassMethods
+  include Paramable::InstanceMethods
+  include Memorable::InstanceMethods
+
 
   @@songs = []
 
-  def initialize
-    @@songs << self
-  end
-
-  def self.find_by_name(name)
-    @@songs.detect{|a| a.name == name}
-  end
+  # def initialize
+  #   @self.class.all << self
+  # end
 
   def artist=(artist)
     @artist = artist
   end
-
-  def self.reset_all
-    @@songs.clear
-  end
-
-  def to_param
-    name.downcase.gsub(' ', '-')
-  end
-
-  def self.count
-    self.all.count
-  end
+  #associates an artist with the new song
 
   def self.all
     @@songs
   end
+
+
+
+  # def to_param
+  #   name.downcase.gsub(' ', '-')
+  # end
+#again I suspect a filenmae ting 
+
 end
